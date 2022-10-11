19a4ab6b13c70211c3a0263ac0fdeb0dc2518567
bellaismail <90236210+bellaismail@users.noreply.github.com>
2022-10-11 15:12:56 +0200

    create userModel and design Drawer

--- lib/repositories/product_repositories/product_repo_api.dart
+++ lib/repositories/product_repositories/product_repo_api.dart
@@ -7,16 +7,17 @@ class APIRepo extends Repository {
   DataConnectionEnum? get dataConnectionEnum => DataConnectionEnum.interNetData;
 
   @override
-  Future<List<ProductModel>> getProductList() async{
+  Future<List<ProductModel>> getProductList() async {
     //for testing dependency injection
     return [
       ProductModel(
-          id: 1,
-          price: 200,
-          title: "Camera",
-          subTitle: "camera with high sensitivety",
-          description: "sdmsdsdsd",
-          image: "https://freepngimg.com/thumb/camera/24673-5-photo-camera-thumb.png"),
+        id: 1,
+        price: 200,
+        title: "Camera",
+        subTitle: "camera with high sensitivety",
+        description: "sdmsdsdsd",
+        image: "https://freepngimg.com/thumb/camera/24673-5-photo-camera-thumb.png",
+      ),
     ];
   }
 }
